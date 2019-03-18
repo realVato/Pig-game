e8edfd1498b672a2e7c325c190ce2771131c6bcd
realVato <44781061+realVato@users.noreply.github.com>
2019-03-18 12:49:04 +0200

    reset numbers through js

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ score = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 document.querySelector('.dice').style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
